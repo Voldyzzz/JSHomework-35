a3d8d50d4221d3fa3ba71beea3ec4b2b564022ad
Voldyzzz <volodymyr.liuliev2000@gmail.com>
2023-12-14 18:59:58 +0200

    Create function for negative solution
    -Update script.js (add getCurrencyFromUser function)

--- script.js
+++ script.js
@@ -40,6 +40,8 @@ getMoney
       return selectCurrency();
     },
     function () {
+      showAvaliableCurrency();
+      getAmountFromUser();
       return Promise.reject();
     }
   )
@@ -59,19 +61,91 @@ function selectCurrency() {
   for (key in userData) {
     currencyFromUserData += `${String(key)},`;
   }
-  const newstr = currencyFromUserData.slice(0, -1);
+  const currencies = currencyFromUserData.slice(0, -1);
 
-  let selectedCurrency = "";
-  do {
-    selectedCurrency = prompt(
-      `Введіть валюту за якою буде виведено баланс, можливі валюти: ${newstr}`,
-      ""
-    );
-  } while (!newstr.includes(selectedCurrency) || selectedCurrency == "");
+  const text = "Введіть валюту за якою буде виведено баланс, можливі валюти:";
+  const selectedCurrency = getCurrencyFromUser(currencies, text);
 
   return selectedCurrency;
 }
 
 function showBalance(currency) {
   alert(`Баланс становить: ${userData[currency]} ${currency}`);
+}
+
+function getCurrencyFromUser(currencies, textInPrompt) {
+  let selectedCurrency = "";
+  do {
+    selectedCurrency = prompt(`${textInPrompt} ${currencies}`, "");
+  } while (!currencies.includes(selectedCurrency) || selectedCurrency == "");
+
+  return selectedCurrency;
+}
+
+function getAmountFromUser() {
+  let sum = 0;
+  do {
+    sum = +prompt(`Сума зняття`, "");
+  } while (isNaN(sum) || sum == "");
+
+  return sum;
+}
+
+function showAvaliableCurrency() {
+  let avaliableCurrency = "";
+  for (key in userData) {
+    for (key2 in bankData) {
+      if (key === key2) {
+        if (bankData[key2]["max"] !== 0) {
+          avaliableCurrency += `${String(key)},`;
+        }
+      }
+    }
+  }
+  const newAvaliableCurrencies = avaliableCurrency.slice(0, -1);
+
+  const text = "Введіть валюту за якою буде списано готівку, можливі валюти:";
+  const selectedCurrency = getCurrencyFromUser(newAvaliableCurrencies, text);
+
+  return selectedCurrency;
+}
+
+function checkAvailableBalance(amount, currency) {
+  let IsUserDataBalance = false;
+  let IsMaxLimit = false;
+  let IsMinLimit = false;
+
+  if (amount <= userData[currency]) {
+    IsUserDataBalance = true;
+    if (amount <= bankData[currency]["max"]) {
+      IsMaxLimit = true;
+    } else {
+      alert(
+        "Доступна сума банку більша за доступну. Максимальна сума зняття: ",
+        bankData[currency]["max"]
+      );
+    }
+    if (amount >= bankData[currency]["min"]) {
+      IsMinLimit = true;
+    } else {
+      alert(
+        "Мінімальна сума банку менша за доступну. Мінімальна сума зняття: ",
+        bankData[currency]["min"]
+      );
+    }
+  } else {
+    alert(
+      "Введена сума користувача більша за доступну. Максимальна сума зняття: ",
+      userData[currency]
+    );
+  }
+
+  if (
+    IsUserDataBalance === true &&
+    IsMaxLimit === true &&
+    IsMinLimit === true
+  ) {
+    return true;
+  }
+  return false;
 }
